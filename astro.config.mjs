80f8db90f8e8816283d14b466f44f682bb0c2a9f
Tobeworks <hallo@tobeworks.de>
2022-09-19 14:19:06 +0200

    Images, Mockups, Ayur added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import vue from "@astrojs/vue";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue()],
-  vite:{
-    
-  }
+  integrations: [vue(), image()],
+  vite: {}
 });
