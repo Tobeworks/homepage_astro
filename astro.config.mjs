ce052c587715844c352a14bd21a7515c0eb864c7
Tobeworks <hallo@tobeworks.de>
2023-03-06 09:44:39 +0100

    Compressor, Content, Text

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,19 @@
 import { defineConfig } from 'astro/config';
 import vue from "@astrojs/vue";
 import image from "@astrojs/image";
-
+import compress from "astro-compress";
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   markdown: {
-    drafts: false,
+    drafts: false
   },
   integrations: [vue(), image({
     // supported levels: 'debug' | 'info' | 'warn' | 'error' | 'silent'
     // default: 'info'
     logLevel: 'debug',
     serviceEntryPoint: '@astrojs/image/sharp'
-  }), react()],
+  }), react(), compress()],
   vite: {}
 });
