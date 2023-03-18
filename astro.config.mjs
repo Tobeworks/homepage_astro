94286c688f14e2740601ede9e5641956b4f13a98
Tobeworks <hallo@tobeworks.de>
2023-03-18 14:04:46 +0100

    Syntax Highlighing in MD files

--- astro.config.mjs
+++ astro.config.mjs
@@ -23,7 +23,18 @@ const compressConfig = {
 // https://astro.build/config
 export default defineConfig({
   markdown: {
-    drafts: false
+    drafts: false,
+    shikiConfig: {
+      // Choose from Shiki's built-in themes (or add your own)
+      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+      theme: 'dark-plus',
+      // Add custom languages
+      // Note: Shiki has countless langs built-in, including .astro!
+      // https://github.com/shikijs/shiki/blob/main/docs/languages.md
+      langs: ['php','javascript','css'],
+      // Enable word wrap to prevent horizontal scrolling
+      wrap: false,
+    },
   },
   site: 'https://tobeworks.de',
   integrations: [vue(), image({
